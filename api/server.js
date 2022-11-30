cd610692dc30cc50935a81c73382592afe3a370f
alexjsanchez1020 <alexjsanchez1020@gmail.com>
2022-11-30 18:11:38 -0500

    install express and add server

--- api/server.js
+++ api/server.js
@@ -1,2 +1,10 @@
 // implement your server here
+const express = require("express"); // import the express package
 // require your posts router and connect it here
+const server = express(); // creates the server
+server.listen(5000, () =>
+  console.log("Server running on http://localhost:5000")
+);
+server.get("/", (req, res) => {
+  res.send("Hello from Express");
+});
--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // require your server and launch it here
+
+// require the express npm module, needs to be added to the project using "npm install express"
+const express = require("express");
+
+// creates an express application using the express module
+const server = express();
