8a0ed512f95d1fd433a0db65ae0f2a875bb057b7
alex sanchez <alexjsanchez1020@gmail.com>
2022-11-30 22:37:58 -0500

    add async getbyid

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -14,12 +14,23 @@ router.get('/', (req,res) => {
         res.json(value)
     })
     .catch(err => {
-        res.status(500).json({
-            message: 'dam no bueno'
-        })
+        res.status(500).json({ message: "The posts information could not be retrieved" })
     })
 })
 
+router.get('/:id', async (req,res) => {
+    const {id} = req.params;
+    try {
+       const post = await PostModel.findById(id)
+       if(!post){
+        res.status(404).json({ message: "The post with the specified ID does not exist" })
+       }else{
+        res.status(200).json(post)
+       }
+    }catch(err){
+        res.status(500).json({ message: "The post information could not be retrieved" })
+    }
+})
 
 //this router will hold all our request to the POST endpoint and we will export it at the bottom,
 //using export.modules
