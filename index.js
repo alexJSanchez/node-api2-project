f5ac4eaba5449cc4581fd2b46df6dfe8aaaea623
alex sanchez <alexjsanchez1020@gmail.com>
2022-11-30 19:33:05 -0500

    ad index.js and run server in port 3000

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 // require your server and launch it here
 
+//we import our server we build with express using the require method
+const server = require('./api/server');
+
+server.get('/', (req,res) => {
+    res.send('you did it')
+})
+
+server.listen(3000, () => {
+    console.log('welcome to our page')
+})
