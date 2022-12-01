dc3496a29fa45eb87c1f8e58158682865f7041ae
alex sanchez <alexjsanchez1020@gmail.com>
2022-12-01 02:00:37 -0500

    Update posts-router.js

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -47,6 +47,24 @@ router.post('/', async (req,res) => {
     }
 })
 
+router.post('/:id', async (req,res) => {
+    const {id} = req.params
+    const {title, contents} = req.body;
+    try {
+       if(!id){
+        res.status(404).json({ message: "The post with the specified ID does not exist" })
+       }else if(!title || !contents){
+        res.status(400).json({ message: "Please provide title and contents for the post" })        
+       }else{
+        const post = await PostModel.update(id, req.body)
+        res.status(200).json(post)
+       }
+    }catch(err){
+        res.status(500).json({ message: "The post information could not be modified" })
+    }
+})
+
+
 //this router will hold all our request to the POST endpoint and we will export it at the bottom,
 //using export.modules
 
